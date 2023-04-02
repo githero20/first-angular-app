c9ae25fa6ab771b2949798f14097cf9f47013df6
Omogbai Atakpu <atakpuomogbai@gmail.com>
2023-04-02 18:45:33 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      p {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  name = 'Dan';
 }
