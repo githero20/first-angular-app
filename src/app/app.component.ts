d935fad27f7ad353391ac645daf4c14b232a7df6
Omogbai Atakpu <atakpuomogbai@gmail.com>
2023-04-07 21:22:57 +0100

    data binding example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<app-servers></app-servers>`,
+  // suitable for few lines of code
   // styleUrls: ['./app.component.css'],
   styles: [
     `
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selecting by element attribute -->
+  selector: '.app-servers',
+  // selecting by class name
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
-
-}
+export class ServersComponent {}
