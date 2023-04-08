2cc06718fa954caff6ee98d31878642b6f373273
Omogbai Atakpu <atakpuomogbai@gmail.com>
2023-04-08 01:47:42 +0100

    more basic examples

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = 'Test Server';
+  serverCreated = false;
+  servers = ['Test Server', 'Test Server 1'];
+
+  constructor() {
+    setTimeout(() => {
+      // 'this' works differently in arrow functions and normal ones.
+      this.allowNewServer = true;
+    }, 3000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created successfully! Name is: ' + this.serverName;
+  }
+
+  onUpdateServerName = (event: Event) => {
+    // explicit casting of the type of event.target so TS doesn't throw an error
+    this.serverName = (<HTMLInputElement>event.target).value;
+  };
+}
